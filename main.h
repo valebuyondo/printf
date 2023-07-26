04a8681d4ceeaf1316d1bb50108389f4241eabf5
Maha EL OUARDI <maha600019@gmail.com>
2023-07-26 16:22:27 +0100

    flags

--- main.h
+++ main.h
@@ -19,7 +19,7 @@ typedef struct identifierStruct
 } identifierStruct;
 
 int _putchar(char c);
-int print_int(va_list arg);
+int print_int(va_list arg, char flags)
 int print_unsigned(va_list arg);
 int _printf(const char *format, ...);
 int print_char(va_list arg);
