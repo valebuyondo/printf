454cd231389bd6dfcf75403d7202fffa55fb6d8c
Maha EL OUARDI <maha600019@gmail.com>
2023-07-26 11:15:16 +0100

    binary

--- print_binary.c
+++ print_binary.c
@@ -7,9 +7,8 @@
 void print_binary(unsigned int n, unsigned int *printed)
 {
 	if (n > 1)
-	{
-		*printed += 1;
-		print_binary(n >> 1, printed);
-	}
-	_putchar((n & 1) + '0');
+    		print_binary(n >> 1, printed);
+
+    	_putchar((n & 1) + '0');
+    	(*printed)++;
 }
